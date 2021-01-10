0210f1a85a456b45db68291cbf16f796d51936b7
danielzt94 <danieltockan@gmail.com>
2021-01-10 09:19:50 +0000

    new home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,9 +26,10 @@ const Home = () => {
 
   const [x, updateX] = useState(0)
 
-
   return <div>
-    <h1>For a look for the whole market tracker, click <a href="https://danieltockan.github.io/crypto-tracker/all">here</a></h1>
+    <h3>For a look at today's whole market data, click
+    <Link className="remove-hyperlink2" to={`/crypto-tracker/all`}> here.</Link>
+    </h3>
     <h1>Today's top 5 coins</h1>
 
     <div className="slider">
@@ -58,9 +59,9 @@ const Home = () => {
 export default Home
 
 const arrowR = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-  <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+  <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
 </svg>
 
 const arrowL = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
-  <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+  <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
 </svg>
