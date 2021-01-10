91e2814c05a2d7048de036683a9b73b4f5ce4328
danielzt94 <danieltockan@gmail.com>
2021-01-10 22:03:10 +0000

    1

--- src/components/CryptoTracker.js
+++ src/components/CryptoTracker.js
@@ -40,7 +40,7 @@ const CryptoTracker = () => {
         <div className="header-button"
           onClick={(event) => updateResultsPerPage(event.target.value)}>
           <span>
-          <h5>Number of results:</h5>
+            <h5>Number of results:</h5>
           </span>
           <button value="10">10</button>
           <button value="25">25</button>
@@ -52,25 +52,29 @@ const CryptoTracker = () => {
       <div className="container">
         {crypto.map((crypto, index) => {
           return <div className="tracker" key={index}>
-            <div className="coin-row">
-              <div className="coin">
-                <img src={crypto.image} alt="" className="symbol" />
-                <h1 className="coin-name">{crypto.name}</h1>
-                <p className="ticker">{crypto.symbol.toUpperCase()}</p>
+            <Link
+              className="remove-hyperlink"
+              to={`/crypto-tracker/${crypto.id}`}>
+              <div className="coin-row">
+                <div className="coin">
+                  <img src={crypto.image} alt="" className="symbol" />
+                  <h1 className="coin-name">{crypto.name}</h1>
+                  <p className="ticker">{crypto.symbol.toUpperCase()}</p>
+                </div>
+                <div className="coin-details">
+                  <p className="coin-price">Price per coin: {crypto.current_price.toLocaleString()}</p>
+                  <p className="volume">Volume traded/24h  {crypto.total_volume.toLocaleString()}</p>
+                  {crypto.price_change_percentage_24h < 0 ? (
+                    <p className="price-change red">{crypto.price_change_percentage_24h.toFixed(2)}%</p>
+                  ) :
+                    (<p className="price-change green">{crypto.price_change_percentage_24h}%</p>)}
+                  <p className="market-cap">Mkt Cap: {crypto.market_cap.toLocaleString()}</p>
+                </div>
               </div>
-              <div className="coin-details">
-                <p className="coin-price">Price per coin: {crypto.current_price.toLocaleString()}</p>
-                <p className="volume">Volume traded/24h  {crypto.total_volume.toLocaleString()}</p>
-                {crypto.price_change_percentage_24h < 0 ? (
-                  <p className="price-change red">{crypto.price_change_percentage_24h.toFixed(2)}%</p>
-                ) :
-                  (<p className="price-change green">{crypto.price_change_percentage_24h}%</p>)}
-                <p className="market-cap">Mkt Cap: {crypto.market_cap.toLocaleString()}</p>
-              </div>
-            </div>
+            </Link>
           </div>
-        
-      })}
+
+        })}
       </div>
     </body>
 
