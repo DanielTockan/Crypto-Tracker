2bfbae577c967572775c1f11de3957fc467fba1b
danielzt94 <danieltockan@gmail.com>
2021-01-10 22:35:02 +0000

    1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Home from './components/Home'
-import Crypto from './components/Crypto'
+import Exchange from './components/Exchange'
 import Forex from './components/Forex'
 import CryptoTracker from './components/CryptoTracker'
 import Navbar from './components/Navbar'
@@ -15,7 +15,7 @@ const App = () => {
   return <BrowserRouter>
     <Navbar />
     <Switch>
-      <Route exact path="/crypto-tracker/crypto" component={Crypto} />
+      <Route exact path="/crypto-tracker/exchange" component={Exchange} />
       <Route exact path="/crypto-tracker/forex" component={Forex} />
       <Route exact path="/crypto-tracker/all" component={CryptoTracker} />
       <Route exact path="/crypto-tracker/:coinId" component={Coin} />
