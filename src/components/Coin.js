d9062e9ca8fd93991f0823d49431025529c0d609
danielzt94 <danieltockan@gmail.com>
2021-01-13 20:58:57 +0000

    coin page complete

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -110,12 +110,12 @@ const Coin = (props) => {
         <h3>Rank: {coin.market_cap_rank}</h3>
         <h3>Price: £ {(coin.market_data.current_price.gbp).toLocaleString()}</h3>
         <h3>Market cap: £ {(coin.market_data.market_cap.gbp).toLocaleString()}</h3>
-        <h3>24 h/high: £ {(coin.market_data.high_24h.gbp).toLocaleString()}</h3>
-        <h3>24 h/low: £ {(coin.market_data.low_24h.gbp).toLocaleString()}</h3>
+        <h3>24 h/high: <span className="green">£ {(coin.market_data.high_24h.gbp).toLocaleString()}</span></h3>
+        <h3>24 h/low: <span className="red">£ {(coin.market_data.low_24h.gbp).toLocaleString()}</span></h3>
         <h3><span>7-day change: </span><span>{coin.price_change_percentage_24h < 0 ? (
           <span className="red">{(coin.market_data.price_change_percentage_7d_in_currency.gbp).toLocaleString()} %</span>
         ) :
-          (<span className="l-green">{(coin.market_data.price_change_percentage_7d_in_currency.gbp).toLocaleString()} %</span>)}</span></h3>
+          (<span className="green">{(coin.market_data.price_change_percentage_7d_in_currency.gbp).toLocaleString()} %</span>)}</span></h3>
         <h3><a href={coin.links.homepage}>Official Website</a></h3>
       </div>
       <div className="main-section">
